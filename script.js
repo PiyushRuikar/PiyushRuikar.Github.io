a841cf3b2c0254c90841aa2934ff6600b4d0be8e
PiyushRuikar <piyushruikar1014@gmail.com>
2024-01-23 23:01:36 +0530

    d

---
+++ script.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $("html").css("scrollBehavior", "auto");
+    });
+
+    $(".navbar .menu li a").click(function () {
+        // applying again smooth scroll on menu items click
+        $("html").css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Java Developer", "Coder", "Problem Solver"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Front-End Developer", "Coder", "Problem Solver", "UI Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    // owl carousel script
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: false,
+        autoplay: false,   //for carousel effect turn it true
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 3,
+                nav: false,
+            },
+        },
+    });
+});
+
+let res = document.getElementById("portfolioDownload")
+res.addEventListener("click", () => {
+    window.open("https://drive.google.com/file/d/1Kc6mMWHNep-eJan4ZsOcARtHedCzEWvx/view?usp=share_link")
+})
+
+let res2 = document.getElementById("portfolioDownload2")
+res2.addEventListener("click", () => {
+    window.open("https://drive.google.com/file/d/1Kc6mMWHNep-eJan4ZsOcARtHedCzEWvx/view?usp=share_link")
+})
+
+
+GitHubCalendar(".calendar", "yashbank");
+// or enable responsive functionality
+GitHubCalendar(".calendar", "yashbank", { responsive: true });
